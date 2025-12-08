64eb8a8660fcecac198e910d10d0b389b5215036
Svitwave <svitlanamaz@gmail.com>
2025-12-08 16:23:35 +0200

    deleted tagsmenu

--- components/TagsMenu/TagsMenu.tsx
+++ components/TagsMenu/TagsMenu.tsx
@@ -1,40 +1,40 @@
-"use client";
+// "use client";
 
-import { useState } from "react";
-import Link from "next/link";
-import css from "./TagsMenu.module.css";
-import { NoteTag } from "@/types/note";
+// import { useState } from "react";
+// import Link from "next/link";
+// import css from "./TagsMenu.module.css";
+// import { NoteTag } from "@/types/note";
 
-const tags: NoteTag[] = ["Work", "Personal", "Meeting", "Shopping", "Todo"];
+// const tags: NoteTag[] = ["Work", "Personal", "Meeting", "Shopping", "Todo"];
 
-export default function TagsMenu() {
-  const [isOpen, setIsOpen] = useState(false);
+// export default function TagsMenu() {
+//   const [isOpen, setIsOpen] = useState(false);
 
-  const tagsMenu = () => setIsOpen(!isOpen);
-  const handleTagClick = () => setIsOpen(false);
+//   const tagsMenu = () => setIsOpen(!isOpen);
+//   const handleTagClick = () => setIsOpen(false);
 
-  return (
-    <div className={css.menuContainer}>
-      <button className={css.menuButton} onClick={tagsMenu}>
-        Notes ▾
-      </button>
-      {isOpen && (
-        <ul className={css.menuList}>
-          <li className={css.menuItem}>
-            <Link href={`/notes/filter/all`} onClick={handleTagClick} className={css.menuLink}>
-              All notes
-            </Link>
-          </li>
+//   return (
+//     <div className={css.menuContainer}>
+//       <button className={css.menuButton} onClick={tagsMenu}>
+//         Notes ▾
+//       </button>
+//       {isOpen && (
+//         <ul className={css.menuList}>
+//           <li className={css.menuItem}>
+//             <Link href={`/notes/filter/all`} onClick={handleTagClick} className={css.menuLink}>
+//               All notes
+//             </Link>
+//           </li>
 
-          {tags.map((tag) => (
-            <li key={tag} className={css.menuItem}>
-              <Link href={`/notes/filter/${tag}`} onClick={handleTagClick} className={css.menuLink}>
-                {tag}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-}
+//           {tags.map((tag) => (
+//             <li key={tag} className={css.menuItem}>
+//               <Link href={`/notes/filter/${tag}`} onClick={handleTagClick} className={css.menuLink}>
+//                 {tag}
+//               </Link>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// }
